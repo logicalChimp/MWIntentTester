3bb05b7160b260646c0dbf5094f24572b4f0b4f2
Geoff Willingham <willinghamg@hotmail.com>
2013-04-14 17:19:15 +0100

    Implements GUI
    GUI buttons for sending Discovery/Start/Stop events
    GUI buttons for sending Button events (button numbers not verified,
    button type fixed at '1')
    Display of incoming Update images / notifications
    Running log on screen of incoming / outgoing events

--- gen/uk/co/mentalspace/android/mwintenttester/R.java
+++ gen/uk/co/mentalspace/android/mwintenttester/R.java
@@ -14,10 +14,23 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_Activate=0x7f070001;
-        public static final int menu_Button=0x7f070003;
-        public static final int menu_Deactivate=0x7f070002;
-        public static final int menu_Discovery=0x7f070000;
+        public static final int menu_Activate=0x7f07000e;
+        public static final int menu_Button=0x7f070010;
+        public static final int menu_Deactivate=0x7f07000f;
+        public static final int menu_Discovery=0x7f07000d;
+        public static final int screen_app_id=0x7f070001;
+        public static final int screen_app_id_label=0x7f070000;
+        public static final int screen_button_activate=0x7f07000a;
+        public static final int screen_button_annouce=0x7f070009;
+        public static final int screen_button_bl=0x7f070004;
+        public static final int screen_button_br=0x7f070008;
+        public static final int screen_button_deactivate=0x7f07000b;
+        public static final int screen_button_ml=0x7f070003;
+        public static final int screen_button_mr=0x7f070007;
+        public static final int screen_button_tl=0x7f070002;
+        public static final int screen_button_tr=0x7f070006;
+        public static final int screen_image=0x7f070005;
+        public static final int screen_log_area=0x7f07000c;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -29,6 +42,18 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int screen_app_id_hint=0x7f040004;
+        public static final int screen_app_id_label=0x7f040003;
+        public static final int screen_button_activate=0x7f04000d;
+        public static final int screen_button_announce=0x7f04000c;
+        public static final int screen_button_bl=0x7f040008;
+        public static final int screen_button_br=0x7f04000b;
+        public static final int screen_button_deactivate=0x7f04000e;
+        public static final int screen_button_ml=0x7f040007;
+        public static final int screen_button_mr=0x7f04000a;
+        public static final int screen_button_tl=0x7f040006;
+        public static final int screen_button_tr=0x7f040009;
+        public static final int screen_image_descriptor=0x7f040005;
     }
     public static final class style {
         /** 
